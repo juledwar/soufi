3db07c2e4e9d5fa83aa1680bec6f0d4b0d4213f8
Nicolas Simonds <nisimond@cisco.com>
2022-05-26 12:17:25 -0700

    Add a status_code attribute to DownloadError exceptions
    One of the more common causes of `DownloadError` exceptions in the
    wild is `HTTPError` exceptions.
    
    Surface the status code from the HTTP response as a convenience to
    callers.
    
    Drive-by: update the version of Black used for tests, for compatibility
    with modern Click.
    
    Fixes Issue #27

--- soufi/exceptions.py
+++ soufi/exceptions.py
@@ -11,6 +11,18 @@ class SourceNotFound(Exception):
 
 
 class DownloadError(Exception):
-    """Raised when source cannot be downloaded."""
+    """Raised when source cannot be downloaded.
 
-    pass
+    This is most commonly raised due to HTTPError exceptions, so this will
+    also surface the status code from any chained HTTP responses, as a
+    convenience.
+    """
+
+    @property
+    def status_code(self):
+        for arg in self.args:
+            try:
+                return arg.response.status_code
+            except AttributeError:
+                continue
+        return None
