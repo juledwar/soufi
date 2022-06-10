e47494c9f55f01126c9e9b93b00b8233359e232b
Nicolas Simonds <nisimond@cisco.com>
2022-06-10 15:07:32 -0700

    Use setuptools_scm instead of PBR
    Also tidies up the builds a bit
    
    Fixes Issue #32

--- setup.py
+++ setup.py
@@ -7,5 +7,11 @@ if setuptools_version < (34, 4):
 
 if __name__ == "__main__":
     setuptools.setup(
-        setup_requires=["pbr>=2.0.0"],
-        pbr=True)
+        # This duplicates the equivalent in pyproject.toml so that setup.py
+        # --version won't report a version of 0.0.0 in editable installs.
+        # setup.py invocations will NOT install build requirements from
+        # pyproject.toml. Unfortunately this also causes
+        # SetuptoolsDeprecationWarnings to be emitted, complaining about
+        # PEP517.
+        setup_requires=["setuptools>=60.5.0", "wheel", "setuptools-scm>=6.4.2"]
+    )
